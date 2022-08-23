fd6af127419e9e7483cdb129efb628cfa5308a19
Shubho <shubhomukherjee194@gmail.com>
2022-08-22 20:29:59 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Shubho Mukherjee on 2021-08-21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
